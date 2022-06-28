19bae22ccda4d3d2af283aae4e50e7daba15eea3
gonghojin <yolip51@naver.com>
2022-06-28 23:39:31 +0900

    Add: 도메인 모델 동작확인

--- data-jpa/src/main/java/com/study/jpa/entity/Member.java
+++ data-jpa/src/main/java/com/study/jpa/entity/Member.java
@@ -1,27 +1,54 @@
 package com.study.jpa.entity;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Access;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
 
 	@Id
 	@GeneratedValue
+	@Column(name = "member_id")
 	private Long id;
-
 	private String username;
+	private int age;
 
-	protected Member() {
-	}
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id")
+	private Team team;
 
 	public Member(String username) {
+		this(username, 0);
+	}
+
+	public Member(String username, int age) {
+		this(username, age, null);
+	}
+
+	public Member(String username, int age, Team team) {
 		this.username = username;
+		this.age = age;
+		if (team != null) {
+			changeTame(team);
+		}
+	}
+
+	private void changeTame(Team team) {
+		this.team = team;
+		team.getMembers().add(this);
 	}
 }
