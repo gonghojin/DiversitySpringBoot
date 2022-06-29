b5b1847de0d269c8fdea2ae34dbd22999c3a4678
gonghojin <yolip51@naver.com>
2022-06-30 00:07:08 +0900

    Fix: @query를 사용한 메서드 정의

--- data-jpa/src/main/java/com/study/jpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/com/study/jpa/repository/MemberRepository.java
@@ -2,10 +2,14 @@ package com.study.jpa.repository;
 
 import com.study.jpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-	List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+	// 아래의 메서드가 없어도
+	@Query("select m from Member m where m.username= :username and m.age = :age")
+	List<Member> findByUsernameAndAgeGreaterThan(@Param("username") String username, @Param("age") int age);
 }
