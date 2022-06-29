743060f8aaa35fa2ac88b2c06ed9980e08186057
gonghojin <yolip51@naver.com>
2022-06-30 00:19:01 +0900

    Add: Query, 값, Dto 조회하기

--- data-jpa/src/main/java/com/study/jpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/com/study/jpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package com.study.jpa.repository;
 
+import com.study.jpa.dto.MemberDto;
 import com.study.jpa.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 	// 아래의 메서드가 없어도
 	@Query("select m from Member m where m.username= :username and m.age = :age")
 	List<Member> findByUsernameAndAgeGreaterThan(@Param("username") String username, @Param("age") int age);
+
+	// 단순히 값 하나를 조회
+	@Query("select m.username from Member m")
+	List<String> findUsernameList();
+
+	@Query("select new com.study.jpa.dto.MemberDto(m.id, m.username, t.name) " + "from Member m join m.team t")
+	List<MemberDto> findMemberDto();
 }
