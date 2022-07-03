8b97d1362250945f8665f827d6bb77394606a8cd
gonghojin <yolip51@naver.com>
2022-07-03 13:04:25 +0900

    Add: web 확장_pageable 사용하기

--- data-jpa/src/main/java/com/study/jpa/controller/HelloController.java
+++ data-jpa/src/main/java/com/study/jpa/controller/HelloController.java
@@ -1,12 +1,39 @@
 package com.study.jpa.controller;
 
+import com.study.jpa.entity.Member;
+import com.study.jpa.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+	private final MemberRepository memberRepository;
+
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello";
+	}
+
+	@GetMapping("/members")
+	public Page<Member> list(Pageable pageable) {
+		Page<Member> page = memberRepository.findAll(pageable);
+		return page;
+	}
+
+
+	@PostConstruct
+	public void init() {
+		for (int i = 0; i < 50; i++) {
+			memberRepository.save(new Member("username" + i));
+		}
 	}
 }
