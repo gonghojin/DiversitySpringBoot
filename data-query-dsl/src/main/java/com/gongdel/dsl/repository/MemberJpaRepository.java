701c3409e0ce3f8a7c45b5698af39c3eaa4c5c6b
gonghojin <yolip51@naver.com>
2022-07-14 23:54:05 +0900

    Add: 순수 JPA 리포지토리

---
+++ data-query-dsl/src/main/java/com/gongdel/dsl/repository/MemberJpaRepository.java
@@ -0,0 +1,42 @@
+package com.gongdel.dsl.repository;
+
+import com.gongdel.dsl.entity.Member;
+import com.querydsl.jpa.JPQLQueryFactory;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberJpaRepository {
+
+	private final EntityManager em;
+	private final JPQLQueryFactory queryFactory;
+
+	public MemberJpaRepository(EntityManager em) {
+		this.em = em;
+		this.queryFactory = new JPAQueryFactory(em);
+	}
+
+	public void save(Member member) {
+		em.persist(member);
+	}
+
+	public Optional<Member> findById(Long id) {
+		Member findMember = em.find(Member.class, id);
+		return Optional.ofNullable(findMember);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	public List<Member> findByUsername(String username) {
+		return em.createQuery("select m from Member m where m.username = :username ", Member.class)
+				.setParameter("username", username)
+				.getResultList();
+	}
+}
